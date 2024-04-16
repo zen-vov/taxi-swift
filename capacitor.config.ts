1c7b20e4c89ec25f404238264a282119045a54b0
zen_vov <zholamanbv@gmail.com>
2024-04-16 22:14:09 +0500

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'taxi-swift',
-  webDir: 'build'
+  appId: "com.example.app",
+  appName: "taxi-swift",
+  webDir: "build",
+  bundledWebRuntime: false,
+  server: {
+    url: "http://172.20.10.4:3000",
+    cleartext: true,
+  },
 };
 
 export default config;
--- src/pages/Choose/ui/Choose.tsx
+++ src/pages/Choose/ui/Choose.tsx
@@ -1,21 +1,29 @@
 import React from "react";
 import Button from "../../../shared/button/ui/button";
+import { Link } from "react-router-dom";
 
-export default function Choose(){
-    return(
-        <div className="flex flex-col items-center justify-center bg-primary" >
-            <h1 className="pt-[107px] mb-[37px] text-3xl text-white font-semibold">
-                Swift
-            </h1>
-            <img  className="w-[295px] h-[170px] mb-[190px]" src="./cars/default-car.png" alt="" />
-           <Button 
-            className="w-[325px] h-[56px] bg-white mb-[30px] rounded-[20px]"
-            title="Driver"
-           />
-           <Button 
-            className="w-[325px] h-[56px] bg-white mb-[30px] rounded-[20px]"
-            title="Passenger"
-           />   
-        </div>
-    );
+export default function Choose() {
+  return (
+    <div className="flex flex-col items-center justify-center bg-primary">
+      <h1 className="pt-[107px] mb-[37px] text-3xl text-white font-semibold">
+        Swift
+      </h1>
+      <img
+        className="w-[295px] h-[170px] mb-[190px]"
+        src="./cars/default-car.png"
+        alt=""
+      />
+      <Link to={"/app/login"}>
+        <Button
+          className="w-[325px] h-[56px] bg-white mb-[30px] rounded-[20px]"
+          title="Driver"
+        />
+      </Link>
+
+      <Button
+        className="w-[325px] h-[56px] bg-white mb-[30px] rounded-[20px]"
+        title="Passenger"
+      />
+    </div>
+  );
 }
